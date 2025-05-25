c051c70c2542e7f0d525c7508549f645bc379b9a
Craig Stark <celstark@gmail.com>
2025-05-25 13:36:37 -0700

    Added toggle_mode to Encoder

--- encoder_tester.py
+++ encoder_tester.py
@@ -18,36 +18,50 @@ encpins = [ [12,13,16],
 
 # Setup a simple class for our encoders
 class Encoder:
-    def __init__(self,knob, button, mode=None, name=None):
+    def __init__(self,knob, button, mode=0, name=None):
         self.knob=knob
         self.button=button
         self.mode=mode
         self.name=name
+        self.steps_last = [0, 0]  # We use these to allow the button to toggle modes
+        self.val_last = [0.0, 0.0]
+    
+    def toggle_mode(self):
+        # store the current readings
+        self.steps_last[self.mode]=self.knob.steps
+        self.val_last[self.mode]=self.knob.steps
+        # toggle the mode
+        if self.mode:
+            self.mode=0
+        else:
+            self.mode=1
+        # bring in the other mode values -- we actually only need to change one as it updates the other
+        self.knob.steps=self.steps_last[self.mode]
     
 knob1 = Encoder(
     RotaryEncoder(a=encpins[0][0], b=encpins[0][1], max_steps=64),
     Button(encpins[0][2], bounce_time = 0.1),
-    None, "Knob 1" )
+    name="Knob 1" )
 
 knob2 = Encoder(
     RotaryEncoder(a=encpins[1][0], b=encpins[1][1], max_steps=64),
     Button(encpins[1][2], bounce_time = 0.1),
-    None, "Knob 2" )
+    name="Knob 2" )
 
 knob3 = Encoder(
     RotaryEncoder(a=encpins[2][0], b=encpins[2][1], max_steps=64),
     Button(encpins[2][2], bounce_time = 0.1),
-    None, "Knob 3" )
+    name="Knob 3" )
 
 knob4= Encoder(
     RotaryEncoder(a=encpins[3][0], b=encpins[3][1], max_steps=64),
     Button(encpins[3][2], bounce_time = 0.1),
-    None, "Knob 4" )
+    name="Knob 4" )
 
 knob5 = Encoder(
     RotaryEncoder(a=encpins[4][0], b=encpins[4][1], max_steps=64),
     Button(encpins[4][2], bounce_time = 0.1),
-    None, "Knob 5" )
+    name="Knob 5" )
 
 
 
@@ -57,11 +71,18 @@ def knob_adjust(dev):
 def button_press(dev):
     print(f'Button: val={dev.value}  devnum={dev.pin.number}')    
 
-def button_hold():
+def button_exit():
     print('Exiting')
     signal.raise_signal(signal.SIGHUP)  # Alt is done.set() here
 
-knob1.button.when_held = button_hold
+def toggle_mode(dev, owner=None):
+    print(f'Button toggle by hold -- before: val={owner.knob.value} steps={owner.knob.steps} devnum={dev.pin.number} name={owner.name}')
+    owner.toggle_mode()
+    print(f'                          after: val={owner.knob.value} steps={owner.knob.steps} devnum={dev.pin.number} name={owner.name}')
+    
+    
+
+knob1.button.when_held = button_exit
 knob1.button.when_pressed = button_press
 knob1.knob.when_rotated = knob_adjust
 
@@ -75,6 +96,7 @@ knob4.button.when_pressed = button_press
 knob4.knob.when_rotated = knob_adjust
 
 knob5.button.when_pressed = button_press
+knob5.button.when_held=lambda: toggle_mode(knob5.button, knob5)
 knob5.knob.when_rotated = knob_adjust
 
 signal.pause()  # This could also have 'done' be an Event() and here call done.wait()
