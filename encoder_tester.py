97dca063b46bf9c96b6671907dca7ecb976e7da9
Craig Stark <celstark@gmail.com>
2025-05-25 12:46:30 -0700

    reworked to use simple class

--- encoder_tester.py
+++ encoder_tester.py
@@ -6,7 +6,7 @@ import sys
 Simple tester to make sure all the knobs are wired up correctly
 ~/camilladsp/.venv/bin/python encoder_tester.py
 
-Either hold a button for >1s or ctrl-c to exit
+Either hold Button1 for >1s or ctrl-c to exit
 
 '''
 
@@ -16,61 +16,65 @@ encpins = [ [12,13,16],
             [24,25,10],
             [27,22,23]]
 
-knob1 = RotaryEncoder(a=encpins[0][0], b=encpins[0][1], max_steps=64)
-button1 = Button(encpins[0][2])
-knob2 = RotaryEncoder(a=encpins[1][0], b=encpins[1][1], max_steps=64)
-button2 = Button(encpins[1][2])
-knob3 = RotaryEncoder(a=encpins[2][0], b=encpins[2][1], max_steps=64)
-button3 = Button(encpins[2][2])
-knob4 = RotaryEncoder(a=encpins[3][0], b=encpins[3][1], max_steps=64)
-button4 = Button(encpins[3][2])
-knob5 = RotaryEncoder(a=encpins[4][0], b=encpins[4][1], max_steps=64)
-button5 = Button(encpins[4][2])
+# Setup a simple class for our encoders
+class Encoder:
+    def __init__(self,knob, button, mode=None, name=None):
+        self.knob=knob
+        self.button=button
+        self.mode=mode
+        self.name=name
+    
+knob1 = Encoder(
+    RotaryEncoder(a=encpins[0][0], b=encpins[0][1], max_steps=64),
+    Button(encpins[0][2], bounce_time = 0.1),
+    None, "Knob 1" )
+
+knob2 = Encoder(
+    RotaryEncoder(a=encpins[1][0], b=encpins[1][1], max_steps=64),
+    Button(encpins[1][2], bounce_time = 0.1),
+    None, "Knob 2" )
+
+knob3 = Encoder(
+    RotaryEncoder(a=encpins[2][0], b=encpins[2][1], max_steps=64),
+    Button(encpins[2][2], bounce_time = 0.1),
+    None, "Knob 3" )
+
+knob4= Encoder(
+    RotaryEncoder(a=encpins[3][0], b=encpins[3][1], max_steps=64),
+    Button(encpins[3][2], bounce_time = 0.1),
+    None, "Knob 4" )
+
+knob5 = Encoder(
+    RotaryEncoder(a=encpins[4][0], b=encpins[4][1], max_steps=64),
+    Button(encpins[4][2], bounce_time = 0.1),
+    None, "Knob 5" )
 
 
-def knob1_adjust(): 
-    print(f'Knob 1  {knob1.steps}')
-def knob2_adjust(): 
-    print(f'Knob 2  {knob2.steps}')
-def knob3_adjust(): 
-    print(f'Knob 3  {knob3.steps}')
-def knob4_adjust(): 
-    print(f'Knob 4  {knob4.steps}')
-def knob5_adjust(): 
-    print(f'Knob 5  {knob5.steps}')
 
-def button1_press():
-    print('Button 1 press')
-def button2_press():
-    print('Button 2 press')
-def button3_press():
-    print('Button 3 press')
-def button4_press():
-    print('Button 4 press')
-def button5_press():
-    print('Button 5 press')
+def knob_adjust(dev): 
+    print(f'Knob: steps={dev.steps}  val={dev.value}   devnum={dev.a.pin} {dev.b.pin}')
+    
+def button_press(dev):
+    print(f'Button: val={dev.value}  devnum={dev.pin.number}')    
 
 def button_hold():
     print('Exiting')
     signal.raise_signal(signal.SIGHUP)  # Alt is done.set() here
 
+knob1.button.when_held = button_hold
+knob1.button.when_pressed = button_press
+knob1.knob.when_rotated = knob_adjust
 
-button1.when_pressed = button1_press
-button2.when_pressed = button2_press
-button3.when_pressed = button3_press
-button4.when_pressed = button4_press
-button5.when_pressed = button5_press
+knob2.button.when_pressed = button_press
+knob2.knob.when_rotated = knob_adjust
 
-button1.when_held = button_hold
-button2.when_held = button_hold
-button3.when_held = button_hold
-button4.when_held = button_hold
-button5.when_held = button_hold
+knob3.button.when_pressed = button_press
+knob3.knob.when_rotated = knob_adjust
 
-knob1.when_rotated = knob1_adjust
-knob2.when_rotated = knob2_adjust
-knob3.when_rotated = knob3_adjust
-knob4.when_rotated = knob4_adjust
-knob5.when_rotated = knob5_adjust
+knob4.button.when_pressed = button_press
+knob4.knob.when_rotated = knob_adjust
+
+knob5.button.when_pressed = button_press
+knob5.knob.when_rotated = knob_adjust
 
 signal.pause()  # This could also have 'done' be an Event() and here call done.wait()
