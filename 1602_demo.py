a3683b99246786f41bb01ee21f543e9c5d8b06f7
Craig Stark <celstark@gmail.com>
2025-08-23 11:18:20 -0700

    Add backlight timeout to demo

--- 1602_demo.py
+++ 1602_demo.py
@@ -1,7 +1,27 @@
 from time import sleep
 import RPi_I2C_driver
+import threading
 
+bl_timer = None
 
+def disable_bl():
+    global bl_status
+    if bl_status:
+        lcd.backlight(0)
+        bl_status=0
+
+def enable_bl():
+    global bl_timer, bl_status
+    try:  
+        bl_timer.cancel()
+    except:
+        pass # We don't actually care if this fails
+    if not bl_status:
+        lcd.backlight(1)
+    bl_timer = threading.Timer(1.0,disable_bl)
+    bl_timer.start()
+    bl_status=1
+        
 frq=440
 q=2.0
 db=3.5
@@ -10,33 +30,38 @@ frq_active=False
 q_active=False
 db_active=True
 symbol_act=chr(126)
+bl_status=1
 
 TL=f"{pot} {frq:5} {db:+1.1f} {q:3.1f}"
 BL=f"    {symbol_act if frq_active else ' '}Hz  {symbol_act if db_active else ' '}dB  {symbol_act if q_active else ' '}Q "
-print(TL)
-print(BL)
 
-mylcd = RPi_I2C_driver.lcd()
-mylcd.lcd_clear()
-mylcd.lcd_display_string(TL, 1)
-mylcd.lcd_display_string(BL, 2)
-sleep(1)
+print('Show the display - should turn off after a sec')
+enable_bl()
+lcd = RPi_I2C_driver.lcd()
+lcd.lcd_clear()
+lcd.lcd_display_string(TL, 1)
+lcd.lcd_display_string(BL, 2)
+sleep(2)
 
+print('Fake some updates and handle the backlight timer')
 db_active=False
 q_active=False
 frq_active=True
+enable_bl()
 for i in range(10):
+    enable_bl()
     frq=i*1000+500
     TL=f"{pot} {frq:5} {db:+1.1f} {q:3.1f}"
     BL=f"    {symbol_act if frq_active else ' '}Hz  {symbol_act if db_active else ' '}dB  {symbol_act if q_active else ' '}Q "
-    mylcd.lcd_display_string(TL, 1)
-    mylcd.lcd_display_string(BL, 2)
-    sleep(0.1)
+    lcd.lcd_display_string(TL, 1)
+    lcd.lcd_display_string(BL, 2)
+    enable_bl()
+    sleep(0.2)
 
-sleep(1)  
-mylcd.backlight(0)
-sleep(1)
-mylcd.backlight(1)  
+
+sleep(2)
+
+
 db_active=True
 q_active=False
 frq_active=False
@@ -44,11 +69,12 @@ for i in range(10):
     db=i*0.5 - 2.0
     TL=f"{pot} {frq:5} {db:+1.1f} {q:3.1f}"
     BL=f"    {symbol_act if frq_active else ' '}Hz  {symbol_act if db_active else ' '}dB  {symbol_act if q_active else ' '}Q "
-    mylcd.lcd_display_string(TL, 1)
-    mylcd.lcd_display_string(BL, 2)
+    lcd.lcd_display_string(TL, 1)
+    lcd.lcd_display_string(BL, 2)
+    enable_bl()
     sleep(0.1)
 
-sleep(1)    
+sleep(2)    
 db_active=False
 q_active=True
 frq_active=False
@@ -56,9 +82,9 @@ for i in range(10):
     q=i*0.5 + 0.5
     TL=f"{pot} {frq:5} {db:+1.1f} {q:3.1f}"
     BL=f"    {symbol_act if frq_active else ' '}Hz  {symbol_act if db_active else ' '}dB  {symbol_act if q_active else ' '}Q "
-    mylcd.lcd_display_string(TL, 1)
-    mylcd.lcd_display_string(BL, 2)
+    lcd.lcd_display_string(TL, 1)
+    lcd.lcd_display_string(BL, 2)
+    enable_bl()
     sleep(0.1)
     
 sleep(2)
-mylcd.backlight(0)
